a09ef2fe16cf1e063d7d3e1c92b9632a6a9b7bfc
Lumine1909 <133463833+Lumine1909@users.noreply.github.com>
2024-08-07 19:21:48 -0400

    init commit

---
+++ src/main/java/io/github/lumine1909/LeavesAddons.java
@@ -0,0 +1,20 @@
+package io.github.lumine1909;
+
+import io.github.lumine1909.config.AddonsConfigHandler;
+import io.github.lumine1909.listener.BotJoinListener;
+import io.github.lumine1909.listener.ShearedWrenchListener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LeavesAddons extends JavaPlugin {
+
+    public static LeavesAddons instance;
+    public static AddonsConfigHandler config;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+        config = new AddonsConfigHandler();
+        new BotJoinListener();
+        new ShearedWrenchListener();
+    }
+}
